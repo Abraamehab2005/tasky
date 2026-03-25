66381021b9fbb79ece41da8d6fbcb7e617d62069
Ebraam Ehab <ibraamehab2005@gmail.com>
2026-03-25 04:14:15 +0200

    Refactor HomeScreen to use HomeController for state management and update task loading logic

---
+++ lib/core/features/home/home_controller.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:tasky/core/constants/storage_key.dart';
+import 'package:tasky/core/services/preferences_manager.dart';
+import 'package:tasky/models/task_model.dart';
+
+class HomeController with ChangeNotifier {
+  HomeController() {
+    init();
+  }
+  List<TaskModel> taskesList = [];
+
+  String? username = "Default";
+  String? userImagePath;
+  int totalTask = 0;
+  int totalDoneTasks = 0;
+  double percent = 0;
+  List<TaskModel> tasks = [];
+  bool isLoading = false;
+
+  void init() {
+    loadTask();
+    loadUserName();
+  }
+
+  void loadTask() async {
+    // isLoading = false;
+    final fianlTask = PreferencesManager().getString("tasks");
+    if (fianlTask != null) {
+      final taskAfterDecode = jsonDecode(fianlTask) as List<dynamic>;
+
+      tasks = taskAfterDecode.map((element) {
+        return TaskModel.fromJson(element);
+      }).toList();
+      calculatePercent();
+    }
+    // isLoading = true;
+    notifyListeners();
+  }
+
+  void loadUserName() async {
+    username = PreferencesManager().getString(StorageKey.username);
+    userImagePath = PreferencesManager().getString('user_image');
+    notifyListeners();
+  }
+
+  calculatePercent() {
+    totalTask = tasks.length;
+    totalDoneTasks = tasks.where((e) => e.isDone).length;
+    percent = totalTask == 0 ? 0 : totalDoneTasks / totalTask;
+    notifyListeners();
+  }
+
+  doneTask(bool? value, int? index) async {
+    tasks[index!].isDone = value ?? false;
+    calculatePercent();
+    final updatedTask = tasks.map((element) => element.toJson()).toList();
+    await PreferencesManager().setString("tasks", jsonEncode(updatedTask));
+    notifyListeners();
+  }
+
+  deleteTask(int? id) async {
+    if (id == null) return;
+
+    tasks.removeWhere((task) => task.id == id);
+    calculatePercent();
+    // todo : make shared method
+    final updatedTasks = tasks.map((element) => element.toJson()).toList();
+    PreferencesManager().setString('tasks', jsonEncode(updatedTasks));
+    notifyListeners();
+  }
+}
